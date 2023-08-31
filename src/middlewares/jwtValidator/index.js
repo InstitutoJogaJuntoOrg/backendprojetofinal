6d9454b6331cfd5145e9f3206f227d9884f3a425
Mikael Marceniuk <mikael.marceniuk@gmail.com>
2023-08-31 00:10:56 -0300

    feat: Added swagger

--- src/middlewares/jwtValidator/index.js
+++ src/middlewares/jwtValidator/index.js
@@ -3,11 +3,12 @@ const jwt = require("jsonwebtoken");
 const jwtValidator = (req, res, next) => {
   const bearerToken = req.headers.authorization;
   if (!bearerToken)
-    return res.send(403).json({ msg: "Jwt Token not provided." });
+    return res.sendStatus(403).json({ msg: "Jwt Token not provided." });
 
   const tokenJwt = bearerToken.split(" ")[1];
   const jwtPayload = jwt.verify(tokenJwt, process.env.JWT_TOKEN);
-  if (!jwtPayload) return res.send(403).json({ msg: "Jwt token not valid." });
+  if (!jwtPayload)
+    return res.sendStatus(403).json({ msg: "Jwt token not valid." });
 
   req.body.userId = jwtPayload.userId;
 
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ class Server {
     this.loadMiddlewares();
     this.loadRoutes();
     this.loadDbConnection();
+    this.loadSwagger();
   }
 
   loadMiddlewares() {
@@ -31,6 +32,19 @@ class Server {
     } catch (e) {
       console.error("Error in connecting to database: ", e.message);
     }
+  }
+
+  async loadSwagger() {
+    const path = await require("path");
+    const fs = await require("fs");
+    const yaml = await require("yaml");
+    const swaggerUi = await require("swagger-ui-express");
+
+    const swaggerDocPath = path.resolve(__dirname, "..", "swagger.yaml");
+    const swaggerDocFile = fs.readFileSync(swaggerDocPath, "utf8");
+    const swaggerDocument = yaml.parse(swaggerDocFile);
+
+    this.app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   }
 }
 
