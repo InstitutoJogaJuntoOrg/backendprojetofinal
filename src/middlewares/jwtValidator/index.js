42702faa686e8ed5419b0ba1854720ffca2f3e28
mgeambastiane <mgeambastiane@gmail.com>
2023-09-28 17:40:59 -0300

    Fix: jwt token

--- src/middlewares/jwtValidator/index.js
+++ src/middlewares/jwtValidator/index.js
@@ -2,13 +2,25 @@ const jwt = require("jsonwebtoken");
 
 const jwtValidator = (req, res, next) => {
   const bearerToken = req.headers.authorization;
+  console.log
   if (!bearerToken)
     return res.sendStatus(403).json({ msg: "Jwt Token not provided." });
 
   const tokenJwt = bearerToken.split(" ")[1];
-  const jwtPayload = jwt.verify(tokenJwt, process.env.JWT_TOKEN);
-  if (!jwtPayload)
+  try {
+    jwtPayload = jwt.verify(tokenJwt, process.env.JWT_TOKEN);
+    console.log("jwt payload")
+    console.log(bearerToken);
+  } catch (error) {
+    console.log(tokenJwt)
+    console.log(process.env.JWT_TOKEN)
+    return res.status(403).json({ "msg": "Jwt token not valid.",
+    "token": tokenJwt 
+  });
+  }
+    if (!jwtPayload)
     return res.sendStatus(403).json({ msg: "Jwt token not valid." });
+  
 
   req.body.userId = jwtPayload.userId;
 
